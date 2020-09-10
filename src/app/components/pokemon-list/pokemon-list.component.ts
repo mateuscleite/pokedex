039d386a4efee4ff9c0454248d66932a12975d38
mateuscleite <mateus.kastilho@gmail.com>
2020-09-10 10:56:06 -0300

    added pagination

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -4,6 +4,7 @@ import { Pokemon } from './../../classes/Pokemon/pokemon';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { map } from 'rxjs/operators'
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -17,70 +18,107 @@ export class PokemonListComponent implements OnInit {
   currentOffset: number;
   count: number;
   subscription: Subscription;
+  querySubscription: Subscription;
+  currentPage: number
 
   readonly offsetConstant: number = 20;
 
-  constructor(private service: PokemonService) { 
+  constructor(private route: ActivatedRoute, private router: Router, private service: PokemonService) { 
     this.isLoading = true;
     this.currentOffset = 0;
   }
   
   ngOnInit() {
     this.isLoading = true;
-    this. subscription = this.service.getPokemonList(this.currentOffset)
+
+    this.querySubscription = this.getCurrentPage();
+
+    this.subscription = this.loadPokemons();   
+  } 
+
+  loadPokemons(){
+    return this.service.getPokemonList(this.currentOffset)
       .pipe(
         map((response : any) => {
-          this.pokemons = response.results
-          this.count = response.count
+          this.pokemons = response['results']
+          this.count = parseInt(response['count'])
         })
       )
       .subscribe(list => {
-        for(let pokemon of this.pokemons){
-          pokemon.id = this.getPokemonId(pokemon.url)
-          pokemon.image = `${environment.DEFAULT_IMAGE}${pokemon.id}.png`
+        //if the user tries to access manually a page that is out of range, send him to the 
+        //last page (if the page user tries to access is higher than the last page) or the
+        //first page (is it is lower than the first page)
+        if(this.currentPage > this.count/this.offsetConstant){
+          this.sendToLastPage();
         }
-        this.isLoading = false;
-      })    
-  } 
+        if(this.currentPage < 0){
+          this.sendToFirstPage();
+        }
+
+        //if the page is within the possible bounds, load the pokémons from that page
+        else{
+          for(let pokemon of this.pokemons){
+            pokemon.id = this.getPokemonId(pokemon.url)
+            pokemon.image = `${environment.DEFAULT_IMAGE}${pokemon.id}.png`
+          }
+          this.isLoading = false;
+        }
+      });
+  }
+
+  getCurrentPage(){
+    return this.route.queryParams.subscribe(queryParams => {
+
+      //if the user types /pokemon on the url we send him/her to the first page of the pokémon list
+      if(Object.keys(queryParams).length === 0){
+        this.currentPage = 0;
+        this.router.navigate(['/pokemon'], {queryParams: {page: this.currentPage}});
+      }
+
+      this.currentPage = parseInt(queryParams['page'])
+  
+      this.currentOffset = this.currentPage*this.offsetConstant;
+    })
+  }
 
   getNextPage(){
     this.isLoading = true;
+    this.currentPage++;
+    this.currentOffset = this.currentPage*this.offsetConstant;
     if(this.currentOffset > this.count){
       return
     }
-    this.currentOffset = this.currentOffset + this.offsetConstant;
+    this.router.navigate(['/pokemon'], {queryParams: {page: this.currentPage}}); 
 
-    this.subscription = this.service.getPokemonList(this.currentOffset)
-      .pipe(
-        map((response : any) => this.pokemons = response.results)
-      )
-      .subscribe(list => {
-        for(let pokemon of this.pokemons){
-          pokemon.id = this.getPokemonId(pokemon.url)
-          pokemon.image = `${environment.DEFAULT_IMAGE}${pokemon.id}.png`
-        }
-        this.isLoading = false;
-      }) 
+    this.subscription = this.loadPokemons();
   }
 
   getPreviousPage(){
-    if(this.currentOffset == 0){
+    this.currentPage--;
+    this.currentOffset = this.currentPage*this.offsetConstant;
+    if(this.currentOffset < 0){
       return
     }
+    this.router.navigate(['/pokemon'], {queryParams: {page: this.currentPage}}); 
     this.isLoading = true;
-    this.currentOffset -= this.offsetConstant;
 
-    this. subscription = this.service.getPokemonList(this.currentOffset)
-      .pipe(
-        map((response : any) => this.pokemons = response.results)
-      )
-      .subscribe(list => {
-        for(let pokemon of this.pokemons){
-          pokemon.id = this.getPokemonId(pokemon.url)
-          pokemon.image = `${environment.DEFAULT_IMAGE}${pokemon.id}.png`
-        }
-        this.isLoading = false;
-      }) 
+    this.subscription = this.loadPokemons();
+  }
+
+  sendToLastPage(){
+    this.currentPage = Math.floor(this.count/this.offsetConstant);
+    this.currentOffset = this.currentPage*this.offsetConstant;
+    console.log('Offset: ' + this.currentOffset + ' Page: ' + this.currentPage)
+    this.subscription = this.loadPokemons();
+    this.router.navigate(['/pokemon'], {queryParams: {page: this.currentPage}});
+  }
+
+  sendToFirstPage(){
+    this.currentPage = 0;
+    this.currentOffset = this.currentPage*this.offsetConstant;
+    console.log('Offset: ' + this.currentOffset + ' Page: ' + this.currentPage)
+    this.subscription = this.loadPokemons();
+    this.router.navigate(['/pokemon'], {queryParams: {page: this.currentPage}});
   }
 
   getPokemonId(url: string){
@@ -91,6 +129,7 @@ export class PokemonListComponent implements OnInit {
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+    this.querySubscription.unsubscribe();
   }
   
 }
